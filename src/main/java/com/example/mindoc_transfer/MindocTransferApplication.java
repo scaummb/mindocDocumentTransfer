e1f1e9d2686696021a33899821072859d8c9215b
scaummb <201330290918a>
2020-07-01 00:01:41 +0800

    初始化：springboot template

---
+++ src/main/java/com/example/mindoc_transfer/MindocTransferApplication.java
@@ -0,0 +1,13 @@
+package com.example.mindoc_transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindocTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindocTransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mindoc_transfer/MindocTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mindoc_transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindocTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
